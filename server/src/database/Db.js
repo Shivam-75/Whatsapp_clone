3e6cb3945c8ba46a3db1b0c0a26d81fcb165fc9d
shivam-75 <shivampandey.7518200305@gmail.com>
2026-05-04 10:59:40 +0530

    fix: cors and db caching

--- server/src/database/Db.js
+++ server/src/database/Db.js
@@ -1,15 +1,17 @@
-
 import mongoose from "mongoose";
 
-
+let cachedConn = null;
 
 const Db = async () => {
-
+  if (cachedConn) {
+    return cachedConn;
+  }
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_DB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-
+    cachedConn = conn;
+    return conn;
   } catch (err) {
     console.error(`MongoDB Error: ${err.message}`);
     throw err;
